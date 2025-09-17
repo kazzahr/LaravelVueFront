c0c69ab1a6ce4fdc061d7cec82a5cf8e1b8c9308
kazzahr <kazzahr@mail.ru>
2025-09-17 23:46:39 +0300

    04 add axios request

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,4 +1,28 @@
 <script setup lang="ts">
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL = "http://localhost:8000";
+
+
+const register = async () => {
+    console.log('Registering....');
+    await axios.get("/sanctum/csrf-cookie")
+    try {
+        const response = await axios.post('/api/register', {
+            name: 'John',
+            email: 'john@email.com',
+            password: 'password',
+            password_confirmation: 'password'
+        });
+        console.log(response.data)
+    } catch (error) {
+        console.error(error)
+    }
+};
+
+register();
 </script>
 <template>
     <h1>Register</h1>
